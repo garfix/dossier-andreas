0b290579dabc0fd8ea1311814e25bb0d96e2f2bc
Patrick van Bergen <patrick.vanbergen@gmail.com>
2017-12-30 21:09:17 +0100

    Inktober, Dérives 2, Sherpa editions of final Capricorne albums

--- site/andreas/php/settings.php
+++ site/andreas/php/settings.php
@@ -2,6 +2,6 @@
 $GLOBALS['DB_server'] = 'localhost';
 $GLOBALS['DB_database'] = 'andreas';
 $GLOBALS['DB_user'] = 'root';
-$GLOBALS['DB_password'] = '';
+$GLOBALS['DB_password'] = 'root';
 
 $GLOBALS['Connection_UTF8'] = false;
